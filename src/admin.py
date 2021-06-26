9e29025c31bf74797f81c9007fbc7986efaebc1a
Diana Ware <dianasofia.ware@gmail.com>
2021-06-26 07:36:53 +0000

    Made a new repository as previous one was giving errors. Adding relationships

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User
+from models import db, User, First_gen, Second_gen, Third_gen
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -11,6 +11,9 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(First_gen, db.session))
+    admin.add_view(ModelView(Second_gen, db.session))
+    admin.add_view(ModelView(Third_gen, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/models.py
+++ src/models.py
@@ -17,3 +17,80 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class First_gen(db.Model):
+    __tablename__ = 'first_gen'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    last_name = db.Column(db.String(120), unique=True, nullable=False)
+    age = db.Column(db.String(3), unique=False, nullable=False)
+    children = db.relationship("Second_gen", backref="parent_owner")
+    grand_children = db.relationship("Third_gen", backref="grandpa_owner")
+    # second_gen = db.relationship('second_gen',backref='first_gen', lazy=True)
+    # third_gen = db.relationship('third_gen', backref='first_gen', lazy=True)
+    # children_id = db.Column(db.Integer, db.ForeignKey('second_gen.id'), nullable=False)
+    # grandchildren_id = db.Column(db.Integer, db.ForeignKey('third_gen.id'), nullable=False)
+
+    def __repr__(self):
+        return '<first_gen %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name,
+            "age": self.age,
+            # "second_gen": list(map(lambda x: x.serialize(), self.second_gen)),
+            # "third_gen": list(map(lambda x: x.serialize(), self.third_gen)),
+        }
+
+class Second_gen(db.Model):
+    __tablename__ = 'second_gen'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    last_name = db.Column(db.String(120), unique=True, nullable=False)
+    age = db.Column(db.String(3), unique=False, nullable=False)
+    parent_id = db.Column(db.Integer, db.ForeignKey('first_gen.id'))
+    children = db.relationship("Third_gen", backref="parent_owner")
+    # children_id = db.Column(db.Integer, db.ForeignKey('third_gen.id'), nullable=False)
+    # third_gen = db.relationship('third_gen', backref='second_gen', lazy=True)
+    # first_gen = db.relationship('first_gen', backref='second_gen', lazy=True)
+
+    def __repr__(self):
+        return '<second_gen %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name,
+            "age": self.age,
+            # "third_gen": list(map(lambda x: x.serialize(), self.third_gen)),
+            # "first_gen": list(map(lambda x: x.serialize(), self.first_gen)),
+        }
+
+class Third_gen(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    age = db.Column(db.String(3), unique=False, nullable=False)
+    grandparent_id = db.Column(db.Integer, db.ForeignKey('first_gen.id'))
+    parent_id= db.Column(db.Integer, db.ForeignKey('second_gen.id'))
+    # grandparent_id = db.Column(db.Integer, db.ForeignKey('first_gen.id'), nullable=False)
+    # first_gen = db.relationship('third_gen', backref='first_gen', lazy=True)
+    # second_gen = db.relationship('first_gen', backref='second_gen', lazy=True)
+
+    def __repr__(self):
+        return '<third_gen %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name,
+            "age": self.age,
+            # "second_gen": list(map(lambda x: x.serialize(), self.second_gen)),
+            # "first_gen": list(map(lambda x: x.serialize(), self.first_gen)),
+
+            # do not serialize the password, its a security breach
+        }
